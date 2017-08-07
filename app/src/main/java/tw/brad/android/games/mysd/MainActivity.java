f7df40501ab1f6e82f232ac11af93d01413310d7
Brad <brad@brad.tw>
2017-08-07 21:06:10 +0800

    2017080702

--- app/src/main/java/tw/brad/android/games/mysd/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/mysd/MainActivity.java
@@ -1,13 +1,50 @@
 package tw.brad.android.games.mysd;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean isPermissionOK;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            // no
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else {
+            isPermissionOK = true;
+            init();
+        }
+
+    }
+
+    private void init(){
+        if (!isPermissionOK) finish();
+        //Log.i("brad", "start");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                isPermissionOK = true;
+
+            }
+            init();
+        }
     }
 }
