e1569ef2f91bb8d4f7c28e169961a3bc58d7fd8e
Brad <brad@brad.tw>
2017-08-09 19:24:27 +0800

    2017080705

--- app/src/main/java/tw/brad/android/games/mysd/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/mysd/MainActivity.java
@@ -100,6 +100,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void test3(){
+
+    }
+    public void test4(){
+
+    }
+    public void test5(){
+
+    }
+    public void test6(){
+
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
---
+++ app/src/main/java/tw/brad/android/games/mysd/MyDBHelper.java
@@ -0,0 +1,29 @@
+package tw.brad.android.games.mysd;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2017/8/9.
+ */
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private final String createTable =
+            "create table cust (_id integer,cname text,tel text,birthday date)";
+    public MyDBHelper(Context context, String name,
+                      SQLiteDatabase.CursorFactory factory,
+                      int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
